3215e183057846ccee0d75fab879744db9c109e7
liaisontw <liaison.tw@gmail.com>
2023-10-05 11:56:55 +0800

    Add view model factory in result fragment

--- app/src/main/java/io/liaison/guessinggame/ResultFragment.kt
+++ app/src/main/java/io/liaison/guessinggame/ResultFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import io.liaison.guessinggame.databinding.FragmentResultBinding
 
 class ResultFragment : Fragment() {
     private var _binding: FragmentResultBinding? = null
     private val binding get() = _binding!!
+    lateinit var viewModel: ResultViewModel
+    lateinit var viewModelFactory: ResultViewModelFactory
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,7 +22,13 @@ class ResultFragment : Fragment() {
         _binding = FragmentResultBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        binding.wonLost.text = ResultFragmentArgs.fromBundle(requireArguments()).result
+        val result = ResultFragmentArgs.fromBundle(requireArguments()).result
+        viewModelFactory = ResultViewModelFactory(result)
+        viewModel = ViewModelProvider(this, viewModelFactory)
+            .get(ResultViewModel::class.java)
+
+        //binding.wonLost.text = ResultFragmentArgs.fromBundle(requireArguments()).result
+        binding.wonLost.text = viewModel.result
         binding.newGameButton.setOnClickListener {
             view.findNavController().navigate(R.id.action_resultFragment_to_gameFragment)
         }
---
+++ app/src/main/java/io/liaison/guessinggame/ResultViewModel.kt
@@ -0,0 +1,7 @@
+package io.liaison.guessinggame
+
+import androidx.lifecycle.ViewModel
+
+class ResultViewModel(finalResult: String) : ViewModel() {
+    val result = finalResult
+}
