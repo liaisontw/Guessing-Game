0bdb4e5026a6902a6931695aca10e9fda78750b1
liaisontw <liaison.tw@gmail.com>
2023-10-06 22:49:26 +0800

    Use private live data in game fragment & game view model

--- app/src/main/java/io/liaison/guessinggame/GameFragment.kt
+++ app/src/main/java/io/liaison/guessinggame/GameFragment.kt
@@ -31,18 +31,19 @@ class GameFragment : Fragment() {
             Observer { newValue -> binding.lives.text = "You have $newValue lives left" })
         viewModel.secretWordDisplay.observe(viewLifecycleOwner,
             Observer { newValue -> binding.word.text = newValue })
+        viewModel.gameOver.observe(viewLifecycleOwner,
+            Observer { newValue ->
+                if (newValue) {
+                    val action = GameFragmentDirections
+                        .actionGameFragmentToResultFragment(viewModel.wonLostMessage())
+                    view.findNavController().navigate(action)
+                }
+            })
 
         binding.guessButton.setOnClickListener() {
             viewModel.makeGuess(binding.guess.text.toString().uppercase())
             binding.guess.text = null
-
-            if (viewModel.isWon() || viewModel.isLost()) {
-                val action = GameFragmentDirections
-                    .actionGameFragmentToResultFragment(viewModel.wonLostMessage())
-                view.findNavController().navigate(action)
-            }
         }
-
         return view
     }
 
--- app/src/main/java/io/liaison/guessinggame/GameViewModel.kt
+++ app/src/main/java/io/liaison/guessinggame/GameViewModel.kt
@@ -1,21 +1,27 @@
 package io.liaison.guessinggame
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    val words = listOf("Android", "Activity", "Fragment")
-    val secretWord = words.random().uppercase()
-    val secretWordDisplay = MutableLiveData<String>()
-    var correctGuesses = ""
-    val incorrectGuesses = MutableLiveData<String>("")
-    val livesLeft = MutableLiveData<Int>(8)
+    private val words = listOf("Android", "Activity", "Fragment")
+    private val secretWord = words.random().uppercase()
+    private val _secretWordDisplay = MutableLiveData<String>()
+    val secretWordDisplay: LiveData<String> get() = _secretWordDisplay
+    private var correctGuesses = ""
+    private val _incorrectGuesses = MutableLiveData<String>("")
+    val incorrectGuesses: LiveData<String> get() = _incorrectGuesses
+    private val _livesLeft = MutableLiveData<Int>(8)
+    val livesLeft: LiveData<Int> get() = _livesLeft
+    private val _gameOver = MutableLiveData<Boolean>(false)
+    val gameOver: LiveData<Boolean> get() = _gameOver
 
     init {
-        secretWordDisplay.value = deriveSecretWordDisplay()
+        _secretWordDisplay.value = deriveSecretWordDisplay()
     }
 
-    fun deriveSecretWordDisplay() : String {
+    private fun deriveSecretWordDisplay() : String {
         var display = ""
         secretWord.forEach {
             display += checkLetter(it.toString())
@@ -23,7 +29,7 @@ class GameViewModel : ViewModel() {
         return display
     }
 
-    fun checkLetter(str: String) = when (correctGuesses.contains(str)) {
+    private fun checkLetter(str: String) = when (correctGuesses.contains(str)) {
         true -> str
         false -> "_"
     }
@@ -32,17 +38,18 @@ class GameViewModel : ViewModel() {
         if (guess.length == 1) {
             if (secretWord.contains(guess)) {
                 correctGuesses += guess
-                secretWordDisplay.value = deriveSecretWordDisplay()
+                _secretWordDisplay.value = deriveSecretWordDisplay()
             } else {
-                incorrectGuesses.value += "$guess "
-                livesLeft.value = livesLeft.value?.minus(1)
+                _incorrectGuesses.value += "$guess "
+                _livesLeft.value = _livesLeft.value?.minus(1)
             }
         }
+        if (isWon() || isLost()) _gameOver.value = true
     }
 
-    fun isWon() = secretWord.equals(secretWordDisplay.value, true)
+    private fun isWon() = secretWord.equals(secretWordDisplay.value, true)
 
-    fun isLost() = livesLeft.value ?:0 <= 0
+    private fun isLost() = livesLeft.value ?:0 <= 0
 
     fun wonLostMessage() : String {
         var message = ""
